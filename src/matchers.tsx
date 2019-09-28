f0b3af99c1d02086dca6df650ac06405275495b4
Douglas Waltman II <doug@dougwaltman.com>
2019-09-27 22:14:32 -0700

    fix(component): fixes args and type defs for css function calls

--- src/matchers.tsx
+++ src/matchers.tsx
@@ -2,37 +2,19 @@
 
 import { css } from "styled-components";
 
-export const is = (prop: string) => (...args: any) => (props: object) =>
-  props[prop]
-    ? css`
-        ${args}
-      `
-    : "";
+export const is = (prop: string) => (...args: [any]) => (props: object) =>
+  props[prop] ? css(...args) : "";
 
-export const isnt = (prop: string) => (...args: any) => (props: object) =>
-  !props[prop]
-    ? css`
-        ${args}
-      `
-    : "";
+export const isnt = (prop: string) => (...args: [any]) => (props: object) =>
+  !props[prop] ? css(...args) : "";
 
-export const isAny = (prop: string, matches: any[]) => (...args: any) => (
+export const isAny = (prop: string, matches: any[]) => (...args: [any]) => (
   props: object
-) =>
-  matches.includes(props[prop])
-    ? css`
-        ${args}
-      `
-    : "";
+) => (matches.includes(props[prop]) ? css(...args) : "");
 
-export const isntAny = (prop: string, matches: any[]) => (...args: any) => (
+export const isntAny = (prop: string, matches: any[]) => (...args: [any]) => (
   props: object
-) =>
-  !matches.includes(props[prop])
-    ? css`
-        ${args}
-      `
-    : "";
+) => (!matches.includes(props[prop]) ? css(...args) : "");
 
 export const value = (prop: string) => (props: object) => props[prop];
 
@@ -42,20 +24,10 @@ export const swap = (prop: string, yup: any, nope: any) => (props: object) =>
 export const choose = (prop: string, map: object) => (props: object) =>
   map[props[prop]];
 
-export const over = (prop: string, amount: number) => (...args: any) => (
+export const over = (prop: string, amount: number) => (...args: [any]) => (
   props: object
-) =>
-  props[prop] > amount
-    ? css`
-        ${args}
-      `
-    : "";
+) => (props[prop] > amount ? css(...args) : "");
 
-export const under = (prop: string, amount: number) => (...args: any) => (
+export const under = (prop: string, amount: number) => (...args: [any]) => (
   props: object
-) =>
-  props[prop] < amount
-    ? css`
-        ${args}
-      `
-    : "";
+) => (props[prop] < amount ? css(...args) : "");
