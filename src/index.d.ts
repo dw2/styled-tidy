604ab1af6828afd7daddb1bc2553c85e500f823e
Doug Waltman <doug@dougwaltman.com>
2019-10-22 20:45:32 -0700

    fix(component): adds missing type exports
    forgot to add export types for the module

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,3 +4,60 @@ import { string } from "prop-types";
 declare module "styled-components" {
   export interface DefaultTheme {}
 }
+
+declare module "styled-tidy" {
+  export function is(
+    prop: string,
+    value?: any
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function isnt(
+    prop: string,
+    value?: any
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function isAny(
+    prop: string,
+    matches: any[]
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function isntAny(
+    prop: string,
+    matches: any[]
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function value(prop: string): (props: object) => any;
+  export function swap(
+    prop: string
+  ): (yup: any, nope: any) => (props: object) => any;
+  export function choose(prop: string, map: object): (props: object) => any;
+  export function over(
+    prop: string,
+    amount: number
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function under(
+    prop: string,
+    amount: number
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function between(
+    prop: string,
+    from: number,
+    to: number
+  ): (str: any, ...args: any[]) => (props: object) => string;
+  export function minWidth(pixels: number): (...args: any[]) => string;
+  export function maxWidth(pixels: number): (...args: any[]) => string;
+  export function minHeight(pixels: number): (...args: any[]) => string;
+  export function maxHeight(pixels: number): (...args: any[]) => string;
+  export function rem(pixels: number, base?: number): string;
+  export function opacify(color: string, adjust: number): string;
+  export function transparentize(color: string, adjust: number): string;
+  export function flex(
+    direction: string,
+    align: string,
+    justify: string
+  ): string;
+  export function grid(cols: string, colGap: string, rowGap?: string): string;
+  export function position(
+    value: string,
+    top: string,
+    right?: string,
+    bottom?: string,
+    left?: string
+  ): string;
+}
